a80e63fb2efe289e89e2b84e50acfc98d7f566dd
fc1xeon <96582306+fc1xeon@users.noreply.github.com>
2022-12-14 16:01:03 +0800

    Update main.lua

--- src/bin/main/main.lua
+++ src/bin/main/main.lua
@@ -7,7 +7,8 @@ function love.load()
         velocity = {0,0,0,0},
         position = {0,0,0,0},
         friction = 0.22,
-        accel = 0
+        angAccel = 0,
+        thrust = 0
     }
 
     GRIDPAIRS={}
@@ -32,13 +33,11 @@ function love.load()
         end
 
     A = function (w,a,s,d,mouse)
-            local nz = (w and 1 or 0) + (s and -1 or 0)
-            local nx = (d and 1 or 0) + (a and -1 or 0) + mouse
-            local ax = math.min(math.max(nx,-1),1)
-            local az = nz>0  and  1   or (
-                       nz<0  and -1/8 or (
-                       nx==0 and  0   or (
-                       math.abs(nx)/8  )))
+            local nx, nz = math.min( math.max( (d and 1 or 0) + (a and -1 or 0) + mouse , -1 ) , 1 ) , (w and 1 or 0) + (s and -1 or 0)
+            local ax, az = nx , nz>0  and  1   or (
+                                nz<0  and -1/8 or (
+                                nx==0 and  0   or (
+                                math.abs(nx)/8  )))
            return ax, az
         end
 end
@@ -59,7 +58,8 @@ function love.keyreleased(key, scancode)
         velocity = {0,0,0,0},
         position = {0,0,0,0},
         friction = boatstate.friction,
-        accel = 0
+        angAccel = 0,
+        thrust = 0
       }
    elseif key == "1" then
       boatstate.friction = 0.22
@@ -90,16 +90,18 @@ function love.update(dt)
     end
     love.delta = love.delta - dx
     boatstate.angAccel = ax
+    boatstate.thrust = az
 end
 
 function love.draw()
+    local n=1
     local windowWidth, windowHeight = love.graphics.getDimensions()
     local cx, cy = windowWidth/2 , windowHeight/2
     local r, ang = math.sqrt(cx*cx+cy*cy) , boatstate.position[4]*math.pi/180
-    local dx, dy, dr, ax = 1.5*r/10, 2*r/10, r/5, boatstate.angAccel
+    local dx, dy, dr, ax, az = 1.5*r/n/2/16, 2*r/n/2/16, r/n, boatstate.angAccel/2, boatstate.thrust/2
     local R11, R12 =  math.sin(-ang), math.cos(-ang)
     local R21, R22 = -math.cos(-ang), math.sin(-ang)
-    local x0, y0 = ((boatstate.position[1]+0.5)%1)-0.5, ((boatstate.position[3]+0.5)%1)-0.5
+    local x0, y0 = ((boatstate.position[1]/16+0.5)%1)-0.5, ((boatstate.position[3]/16+0.5)%1)-0.5
     love.graphics.setColor(1,1,1)
     for i=1,22 do
         local r1 , r2 , r3 , r4 = GRIDPAIRS[i][1]-x0 , GRIDPAIRS[i][2]-y0 , GRIDPAIRS[i][3]-x0 , GRIDPAIRS[i][4]-y0
@@ -109,14 +111,15 @@ function love.draw()
     end
     love.graphics.print(boatstate.position[1] .. ',' .. boatstate.position[2] .. ',' .. boatstate.position[3] .. ' (' .. boatstate.position[4] .. ')\n' ..
                         boatstate.velocity[1] .. ',' .. boatstate.velocity[2] .. ',' .. boatstate.velocity[3] .. ' (' .. boatstate.velocity[4] .. ')\n' ..
-                        'Press 1/2/3/4 to set friction to Blue/Pack/Air/Block (mu='  .. boatstate.friction .. ')\n' ..
                         'Sensitivity: ' .. 2^(love.sensitivityStep/12) .. ' (step '  .. love.sensitivityStep .. ', scroll to adjust) \n' ..
+                        'Press 1/2/3/4 to set friction to Blue/Pack/Air/Gnd\n' ..
                         'Press R to reset, Esc to exit.')
-    love.graphics.printf(math.sqrt(boatstate.velocity[1]*boatstate.velocity[1]+boatstate.velocity[3]*boatstate.velocity[3]) .. ' m/s',0,cy-dy-15,windowWidth,"center")
     love.graphics.setColor(181/256,153/256,104/256)
     love.graphics.rectangle('fill',cx-dx,cy-dy,2*dx,2*dy)
-    love.graphics.setColor(0,1,0)
-    love.graphics.line(cx,cy,cx+ax*dx,cy)
+    love.graphics.setColor(0,1,1)
+    love.graphics.line(cx,cy,cx+ax*r/n,cy)
     love.graphics.setColor(1,0,0)
-    love.graphics.line(cx,cy-dy,cx,cy+dy)
+    love.graphics.line(cx,cy,cx,cy-az*r/n)
+    love.graphics.setColor(0,1,0)
+    love.graphics.printf(math.sqrt(boatstate.velocity[1]*boatstate.velocity[1]+boatstate.velocity[3]*boatstate.velocity[3]) .. ' m/s\n' .. 'mu=' .. boatstate.friction,0,cy+dy,windowWidth,"center")
 end
